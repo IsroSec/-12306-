30005fe9bccefd220d34fd53a96c2523d4bd6f2c
xikanqi <3051855262@qq.com>
2024-12-20 15:53:11 +0800

    启动类中打印环境信息 增加context-path项目前缀

--- member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
+++ member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
@@ -1,15 +1,24 @@
 package com.jiawa.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.jiawa")
 public class MemberApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class.getName());
+
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication application = new SpringApplication(MemberApplication.class);
+        Environment environment = application.run(args).getEnvironment();
+        LOG.info("启动成功！");
+        LOG.info("MemberApplication: \t http://127.0.0.1:{}/", environment.getProperty("server.port"));
     }
 
 }
