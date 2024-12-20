5f4bf870c507449d1078e202c036b37b67d144ea
xikanqi <3051855262@qq.com>
2024-12-20 14:47:48 +0800

    初始化

---
+++ src/main/java/com/example/train/test.java
@@ -0,0 +1,19 @@
+package com.example.train;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * ClassName: test
+ * Package: com.example.train
+ * Description:
+ *
+ * @Author GalSec
+ * @Create 2024/12/20 14:31
+ * @Version 1.0
+ */
+@Controller
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world123");
+    }
+}
