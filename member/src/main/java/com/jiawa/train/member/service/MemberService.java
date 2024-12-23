b6f46bcd5bcd84f2385138f6c1648195deba854c
xikanqi <3051855262@qq.com>
2024-12-23 16:29:29 +0800

    4.14 集成Mybatis官方代码生成器,访问数据库成功

--- member/src/main/java/com/jiawa/train/member/service/MemberService.java
+++ member/src/main/java/com/jiawa/train/member/service/MemberService.java
@@ -19,6 +19,6 @@ public class MemberService {
     @Autowired
     private MemberMapper memberMapper;
     public int count() {
-        return memberMapper.count();
+        return Math.toIntExact(memberMapper.countByExample(null));
     }
 }
