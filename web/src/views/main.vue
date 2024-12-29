bcc21a8fd87aff893375b8155232f6cd5aaa9d11
xikanqi <3051855262@qq.com>
2024-12-29 19:20:42 +0800

    7.6 制作欢迎页面

--- web/src/views/main.vue
+++ web/src/views/main.vue
@@ -4,15 +4,10 @@
     <a-layout>
       <the-sider></the-sider>
       <a-layout style="padding: 0 24px 24px">
-        <a-breadcrumb style="margin: 16px 0">
-          <a-breadcrumb-item>Home</a-breadcrumb-item>
-          <a-breadcrumb-item>List</a-breadcrumb-item>
-          <a-breadcrumb-item>App</a-breadcrumb-item>
-        </a-breadcrumb>
         <a-layout-content
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
         >
-          所有会员总数：{{count}}
+          <router-view></router-view>
         </a-layout-content>
       </a-layout>
     </a-layout>
@@ -21,14 +16,13 @@
 <script>
 import TheHeader from "@/components/the-header.vue";
 import TheSider from "@/components/the-sider.vue";
-import {defineComponent, ref} from "vue";
+import {defineComponent} from "vue";
 export default defineComponent({
   components: {
     TheSider,
     TheHeader,
   },
   setup(){
-    const count=ref(0);
       // axios.get("/member/member/count").then(response=>{
       //   let data=response.data;
       //   if (data.success){
@@ -38,7 +32,6 @@ export default defineComponent({
       //   }
       // });
       return {
-        count
       };
   }
 })
