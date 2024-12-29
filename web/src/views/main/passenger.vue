d4ad3e495833806d4df8f6ea0eef568785d008f2
xikanqi <3051855262@qq.com>
2024-12-29 19:54:33 +0800

    7.6 乘车人新增界面，添加新增按钮，添加乘车人表单

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -1,12 +1,51 @@
 <template>
-  <h1>乘车人页面</h1>
+  <a-button type="primary" @click="showModel">新增</a-button>
+  <a-modal v-model:visible="visible" title="新增乘客" @ok="handleOk" @cancel="handleCancel"
+  ok-text="保存" cancel-text="取消">
+    <a-form :model="passenger" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
+      <a-form-item label="乘客姓名">
+        <a-input v-model:value="passenger.name" placeholder="请输入乘客姓名" />
+      </a-form-item>
+      <a-form-item label="身份证">
+        <a-input v-model:value="passenger.idCard" />
+      </a-form-item>
+      <a-form-item label="旅客类型">
+        <a-select v-model:value="passenger.type">
+          <a-select-option value="1">成人</a-select-option>
+          <a-select-option value="2">儿童</a-select-option>
+          <a-select-option value="3">学生</a-select-option>
+        </a-select>
+      </a-form-item>
+    </a-form>
+  </a-modal>
 </template>
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent,ref } from 'vue';
 
 export default defineComponent({
   setup() {
+    const visible = ref(false);
+    let passenger = ref({
+      id: undefined,
+      memberId: undefined,
+      name: undefined,
+      idCard: undefined,
+      type: undefined,
+      createTime: undefined,
+      updateTime: undefined,
+    });
+    const showModel=()=>{
+      visible.value=true;
+    }
+    const handleOk=()=>{
+      visible.value=false;
+    }
+
     return {
+      passenger,
+      visible,
+      showModel,
+      handleOk
     };
   },
 });
