6381e31228f200489243d6cbc3e32fc3122b25bc
xikanqi <3051855262@qq.com>
2025-01-01 22:19:09 +0800

    8.9 制作的模板进行测试

--- generator/src/main/java/com/jiawa/train/generator/gen/ServerGenerator.java
+++ generator/src/main/java/com/jiawa/train/generator/gen/ServerGenerator.java
@@ -86,7 +86,8 @@ public class ServerGenerator {
 //        gen(Domain, param,"service","service");
 //        gen(Domain, param,"controller","controller");
 //        gen(Domain, param,"req","saveReq");
-
+        gen(Domain, param, "req", "queryReq");
+        gen(Domain, param, "resp", "queryResp");
 //        FreemarkerUtil.initConfig("test.ftl");
 //        HashMap<String, Object> param = new HashMap<>();
 //        param.put("domain","Test");
