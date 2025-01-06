c71085f3b914eceae5b179067701218265fac30c
xikanqi <3051855262@qq.com>
2025-01-06 14:14:52 +0800

    10.2 增加batch定时调度模块

---
+++ batch/src/main/java/com/jiawa/train/batch/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.jiawa.train.batch.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ClassName: TestController
+ * Package: com.jiawa.train.business.controller
+ * Description:
+ *
+ * @Author GalSec
+ * @Create 2025/1/2 23:49
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/batch")
+public class TestController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
