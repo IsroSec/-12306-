41c8f198625731d6fcc32293cc74495aa3f90ac0
xikanqi <3051855262@qq.com>
2025-01-06 19:58:09 +0800

    10.6 通过控台界面操作定时任务 对菜单做分类改为两级菜单，路由也改为多级路由 修改vue文件位置，保持层级关系和路由一致

--- admin/src/components/the-sider.vue
+++ admin/src/components/the-sider.vue
@@ -2,48 +2,37 @@
   <a-layout-sider width="200" style="background: #fff">
     <a-menu
         v-model:selectedKeys="selectedKeys2"
-        v-model:openKeys="openKeys"
+        :openKeys="['batch', 'base']"
         mode="inline"
         :style="{ height: '100%', borderRight: 0 }"
     >
-      <a-sub-menu key="sub1">
+      <a-menu-item key="/welcome"><router-link to="/welcome">欢迎</router-link></a-menu-item>
+      <a-menu-item key="/about"><router-link to="/about">关于</router-link></a-menu-item>
+      <a-sub-menu key="base">
         <template #title>
               <span>
                 <user-outlined />
-                subnav 1
+                基础数据
               </span>
         </template>
-        <a-menu-item key="/welcome"><router-link to="/welcome">欢迎</router-link></a-menu-item>
-        <a-menu-item key="/about"><router-link to="/about">关于</router-link></a-menu-item>
-        <a-menu-item key="/station"><router-link to="/station">车站管理</router-link></a-menu-item>
-        <a-menu-item key="/train"><router-link to="/train">车次管理</router-link></a-menu-item>
-        <a-menu-item key="/train-station"><router-link to="/train-station">火车管理</router-link></a-menu-item>
-        <a-menu-item key="/train_carriage"><router-link to="/train_carriage">车厢管理</router-link></a-menu-item>
-        <a-menu-item key="/train_seat"><router-link to="/train_seat">座位管理</router-link></a-menu-item>
+        <a-menu-item key="/base/station"><router-link to="/base/station">车站管理</router-link></a-menu-item>
+        <a-menu-item key="/base/train"><router-link to="/base/train">车次管理</router-link></a-menu-item>
+        <a-menu-item key="/base/train-station"><router-link to="/base/train-station">火车管理</router-link></a-menu-item>
+        <a-menu-item key="/base/train_carriage"><router-link to="/base/train_carriage">车厢管理</router-link></a-menu-item>
+        <a-menu-item key="/base/train_seat"><router-link to="/base/train_seat">座位管理</router-link></a-menu-item>
       </a-sub-menu>
-      <a-sub-menu key="sub2">
+      <a-sub-menu key="batch">
         <template #title>
-              <span>
-                <laptop-outlined />
-                subnav 2
-              </span>
+          <span>
+            <UnorderedListOutlined />
+            跑批管理
+          </span>
         </template>
-        <a-menu-item key="5">option5</a-menu-item>
-        <a-menu-item key="6">option6</a-menu-item>
-        <a-menu-item key="7">option7</a-menu-item>
-        <a-menu-item key="8">option8</a-menu-item>
-      </a-sub-menu>
-      <a-sub-menu key="sub3">
-        <template #title>
-              <span>
-                <notification-outlined />
-                subnav 3
-              </span>
-        </template>
-        <a-menu-item key="9">option9</a-menu-item>
-        <a-menu-item key="10">option10</a-menu-item>
-        <a-menu-item key="11">option11</a-menu-item>
-        <a-menu-item key="12">option12</a-menu-item>
+        <a-menu-item key="/batch/job">
+          <router-link to="/batch/job">
+            <MenuUnfoldOutlined /> &nbsp; 任务管理
+          </router-link>
+        </a-menu-item>
       </a-sub-menu>
     </a-menu>
   </a-layout-sider>
@@ -57,15 +46,13 @@ export default defineComponent({
   name: 'the-sider',
   data() {
     const selectedKeys2 = ref([]);
-    const openKeys = ref(['sub1']);
     watch(()=>Router.currentRoute.value.path, (newValue)=>{
       console.log(newValue);
       selectedKeys2.value=[]
       selectedKeys2.value.push(newValue)
     }, {immediate: true})
     return {
-      selectedKeys2,
-      openKeys
+      selectedKeys2
     }
   }
 })
