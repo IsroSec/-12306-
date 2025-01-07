cb68b7e5bbfd303ea1bf3014d510bc725483b542
xikanqi <3051855262@qq.com>
2025-01-07 21:38:55 +0800

    11.3 为每日车次表单增加车次下拉选择，并自动填充车次相关数据

--- admin/src/views/main/business/daily-train.vue
+++ admin/src/views/main/business/daily-train.vue
@@ -38,7 +38,7 @@
         <a-date-picker v-model:value="dailyTrain.date" valueFormat="YYYY-MM-DD" placeholder="请选择日期" />
       </a-form-item>
       <a-form-item label="车次编号">
-        <a-input v-model:value="dailyTrain.code" />
+        <train-select-view v-model="dailyTrain.code" @change="onChangeCode"></train-select-view>
       </a-form-item>
       <a-form-item label="车次类型">
         <a-select v-model:value="dailyTrain.type">
@@ -75,9 +75,11 @@
 import { defineComponent, ref, onMounted } from 'vue';
 import {notification} from "ant-design-vue";
 import axios from "axios";
+import TrainSelectView from "@/components/train-select.vue";
 
 export default defineComponent({
   name: "daily-train-view",
+  components: {TrainSelectView},
   setup() {
     const TRAIN_TYPE_ARRAY = window.TRAIN_TYPE_ARRAY;
     const visible = ref(false);
@@ -231,7 +233,13 @@ export default defineComponent({
         size: page.pageSize
       });
     };
-
+    const onChangeCode = (train) => {
+      console.log("车次下拉组件选择：", train);
+      let t = Tool.copy(train);
+      delete t.id;
+      // 用assign可以合并
+      dailyTrain.value = Object.assign(dailyTrain.value, t);
+    };
     onMounted(() => {
       handleQuery({
         page: 1,
@@ -252,7 +260,8 @@ export default defineComponent({
       onAdd,
       handleOk,
       onEdit,
-      onDelete
+      onDelete,
+      onChangeCode
     };
   },
 });
