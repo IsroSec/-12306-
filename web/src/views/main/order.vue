a39b40b9f87b683b8eeabc86b74896a29d3161e1
xikanqi <3051855262@qq.com>
2025-01-14 09:45:41 +0800

    12.7 订单页面显示座位信息

--- web/src/views/main/order.vue
+++ web/src/views/main/order.vue
@@ -7,6 +7,13 @@
     <span class="order-train-main">——</span>&nbsp;
     <span class="order-train-main">{{dailyTrainTicket.end}}</span>站
     <span class="order-train-main">({{dailyTrainTicket.endTime}})</span>&nbsp;
+    <div class="order-train-ticket">
+      <span v-for="item in seatTypes" :key="item.type">
+        <span>{{item.desc}}</span>：
+        <span class="order-train-ticket-main">{{item.price}}￥</span>&nbsp;
+        <span class="order-train-ticket-main">{{item.count}}</span>&nbsp;张票&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </span>
+    </div>
   </div>
 </template>
 
@@ -17,14 +24,62 @@ export default defineComponent({
   name: 'order-view',
   setup() {
     const dailyTrainTicket = SessionStorage.get(SESSION_ORDER) || {};
-    return {dailyTrainTicket}
+
+    const seatTypes=[];
+    for (let KEY in SEAT_TYPE){
+      let key=KEY.toLowerCase();
+      if (dailyTrainTicket[key]>=0){
+        seatTypes.push({
+          type: KEY,
+          code: SEAT_TYPE[KEY]["code"],
+          desc: SEAT_TYPE[KEY]["desc"],
+          count: dailyTrainTicket[key],
+          price: dailyTrainTicket[key + 'Price'],
+        })
+      }
+    }
+    return {
+      dailyTrainTicket,
+      seatTypes
+    }
   }
 });
 </script>
 
 <style scoped>
-.order-train-main {
+.order-train .order-train-main {
   font-size: 18px;
   font-weight: bold;
+}
+.order-train .order-train-ticket {
+  margin-top: 15px;
+}
+.order-train .order-train-ticket .order-train-ticket-main {
+  color: red;
+  font-size: 18px;
+}
+
+.order-tickets {
+  margin: 10px 0;
+}
+.order-tickets .ant-col {
+  padding: 5px 10px;
+}
+.order-tickets .order-tickets-header {
+  background-color: cornflowerblue;
+  border: solid 1px cornflowerblue;
+  color: white;
+  font-size: 16px;
+  padding: 5px 0;
+}
+.order-tickets .order-tickets-row {
+  border: solid 1px cornflowerblue;
+  border-top: none;
+  vertical-align: middle;
+  line-height: 30px;
+}
+
+.order-tickets .choose-seat-item {
+  margin: 5px 5px;
 }
 </style>
