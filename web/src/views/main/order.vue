185c82f1d69f386c5a33c0d0db69b5f70c600000
xikanqi <3051855262@qq.com>
2025-01-14 09:20:37 +0800

    12.7 增加预定按钮，点击预定时，跳转到下单页面，并使用session

---
+++ web/src/views/main/order.vue
@@ -0,0 +1,21 @@
+<template>
+<p>
+  {{dailyTrainTicket}}
+</p>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: 'order-view',
+  setup() {
+    const dailyTrainTicket = SessionStorage.get("dailyTrainTicket") || {};
+    return {dailyTrainTicket}
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
--- web/src/views/main/ticket.vue
+++ web/src/views/main/ticket.vue
@@ -14,6 +14,7 @@
            :loading="loading">
     <template #bodyCell="{ column, record }">
       <template v-if="column.dataIndex === 'operation'">
+        <a-button type="primary" @click="toOrder(record)">预定</a-button>
       </template>
       <template v-else-if="column.dataIndex === 'station'">
         {{record.start}}<br/>
@@ -78,6 +79,7 @@ import {notification} from "ant-design-vue";
 import axios from "axios";
 import StationSelectView from "@/components/station-select";
 import dayjs from "dayjs";
+import router from "@/router";
 
 export default defineComponent({
   name: "daily-train-ticket-view",
@@ -214,6 +216,10 @@ export default defineComponent({
         dataIndex: 'yw',
         key: 'yw',
       },
+      {
+        title: '操作',
+        dataIndex: 'operation',
+      },
       // {
       //   title: '硬卧票价',
       //   dataIndex: 'ywPrice',
@@ -279,6 +285,11 @@ export default defineComponent({
       return dayjs('00:00:00', 'HH:mm:ss').second(diff).format('HH:mm:ss');
     };
 
+    const toOrder=(record)=>{
+      dailyTrainTicket.value=Tool.copy(record)
+      SessionStorage.set("dailyTrainTicket", dailyTrainTicket.value)
+      router.push("/order")
+    }
     onMounted(() => {
     });
 
@@ -292,7 +303,8 @@ export default defineComponent({
       handleQuery,
       loading,
       params,
-      calDuration
+      calDuration,
+      toOrder
     };
   },
 });
