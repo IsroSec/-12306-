1e8a2fb42ae890c00ed9adb8bbad7d097c8a36be
xikanqi <3051855262@qq.com>
2025-01-16 12:03:29 +0800

    12.15 计算相对第一个位置的偏移值

--- business/src/main/java/com/jiawa/train/business/service/ConfirmOrderService.java
+++ business/src/main/java/com/jiawa/train/business/service/ConfirmOrderService.java
@@ -4,11 +4,13 @@ import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.EnumUtil;
 import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.jiawa.train.business.domain.DailyTrainTicket;
 import com.jiawa.train.business.enums.ConfirmOrderStatusEnum;
+import com.jiawa.train.business.enums.SeatColEnum;
 import com.jiawa.train.business.enums.SeatTypeEnum;
 import com.jiawa.train.business.req.ConfirmOrderTicketReq;
 import com.jiawa.train.common.context.LoginMemberContext;
@@ -29,6 +31,7 @@ import org.springframework.stereotype.Service;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -98,6 +101,7 @@ public class ConfirmOrderService {
         String trainCode = confirmOrderDoReq.getTrainCode();
         String start = confirmOrderDoReq.getStart();
         String end = confirmOrderDoReq.getEnd();
+        List<ConfirmOrderTicketReq> tickets = confirmOrderDoReq.getTickets();
         DateTime now = DateTime.now();
         ConfirmOrder confirmOrder = new ConfirmOrder();
         confirmOrder.setId(SnowUtil.getSnowflakeNextId());
@@ -110,19 +114,51 @@ public class ConfirmOrderService {
         confirmOrder.setEnd(end);
         confirmOrder.setDailyTrainTicketId(confirmOrderDoReq.getDailyTrainTicketId());
         confirmOrder.setStatus(ConfirmOrderStatusEnum.INIT.getCode());
-        confirmOrder.setTickets(JSON.toJSONString(confirmOrderDoReq.getTickets()));
+        confirmOrder.setTickets(JSON.toJSONString(tickets));
         confirmOrderMapper.insert(confirmOrder);
         // 查出余票记录，需要得到真实的库存
         DailyTrainTicket dailyTrainTicket = dailyTrainTicketService.selectByUnique(date, trainCode, start, end);
         LOG.info("余票信息：{}", dailyTrainTicket);
         // 扣减余票数量，并判断余票是否足够
-        for (ConfirmOrderTicketReq ticketReq: confirmOrderDoReq.getTickets()) {
-            String seatTypeCode = ticketReq.getSeatTypeCode();
-            SeatTypeEnum seatTypeEnum = EnumUtil.getBy(SeatTypeEnum::getCode, seatTypeCode);
-            //用反射写写看?
-            reduceTicketsReflect(seatTypeEnum, dailyTrainTicket);
-            //switch方法
-            //reduceTickets(seatTypeEnum, dailyTrainTicket);
+        reduceTickets(tickets, dailyTrainTicket);
+
+        //计算相对第一个位置的偏移值
+        //比如选择的C1，C2,则偏移值是：【0，5】
+        //比如选择的A1B1C1，则偏移值是：[0,1,2]
+        ConfirmOrderTicketReq ticketReq0 = tickets.get(0);
+        if (StrUtil.isNotBlank(ticketReq0.getSeat())){
+            LOG.info("本次购票选座");
+            // 获取座位类型，判断所选座位的偏移值 A C D F或A B C D F
+            List<SeatColEnum> colsByType = SeatColEnum.getColsByType(ticketReq0.getSeatTypeCode());
+            //组建一个偏移值数组
+            //{
+            //       A1: false, C1: true，D1: false, F1: false，
+            //       A2: false, C2: false，D2: true, F2: false
+            //}
+            ArrayList<String> referSeatList = new ArrayList<>();
+            for (int i=1;i<=2;i++){
+                for (SeatColEnum seatColEnum: colsByType){
+                    referSeatList.add(seatColEnum.getCode()+i);
+                }
+            }
+            LOG.info("座位：{}", referSeatList);
+            //找偏移值referSeatList={A1,C1,D1,F1,A2,C2,D2,F2}绝对偏移值减去第一位绝对偏移值等于相对偏移值
+            //获取绝对偏移值
+            ArrayList<Integer> absoluteOffsetList = new ArrayList<>();
+            for (ConfirmOrderTicketReq ticket : tickets) {
+                int index = referSeatList.indexOf(ticket.getSeat());
+                absoluteOffsetList.add(index);
+            }
+            LOG.info("绝对偏移值：{}", absoluteOffsetList);
+            ArrayList<Integer> offsetList = new ArrayList<>();
+            for (Integer index : absoluteOffsetList) {
+                //相对第一个座位偏移值
+                int offset = index - absoluteOffsetList.get(0);
+                offsetList.add(offset);
+            }
+            LOG.info("相对偏移值：{}", offsetList);
+        }else {
+            LOG.info("本次购票不选座");
         }
         // 选座
 
@@ -136,6 +172,47 @@ public class ConfirmOrderService {
         // 余票详情表修改余票；
         // 为会员增加购票记录
         // 更新确认订单为成功
+    }
+
+    private static void reduceTickets(List<ConfirmOrderTicketReq> tickets, DailyTrainTicket dailyTrainTicket) {
+        for (ConfirmOrderTicketReq ticketReq: tickets) {
+            String seatTypeCode = ticketReq.getSeatTypeCode();
+            SeatTypeEnum seatTypeEnum = EnumUtil.getBy(SeatTypeEnum::getCode, seatTypeCode);
+            //用反射写写看?
+//            reduceTicketsReflect(seatTypeEnum, dailyTrainTicket);
+            //switch方法
+            switch (seatTypeEnum){
+                case YDZ -> {
+                    int countLeft = dailyTrainTicket.getYdz() - 1;
+                    if (countLeft < 0){
+                        throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_TICKET_COUNT_ERROR);
+                    }
+                    dailyTrainTicket.setYdz(countLeft);
+                }
+                case EDZ -> {
+                    int countLeft = dailyTrainTicket.getEdz() - 1;
+                    if (countLeft < 0){
+                        throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_TICKET_COUNT_ERROR);
+                    }
+                    dailyTrainTicket.setEdz(countLeft);
+
+                }case YW -> {
+                    int countLeft = dailyTrainTicket.getYw() - 1;
+                    if (countLeft < 0){
+                        throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_TICKET_COUNT_ERROR);
+                    }
+                    dailyTrainTicket.setYw(countLeft);
+
+                }case RW -> {
+                    int countLeft = dailyTrainTicket.getRw() - 1;
+                    if (countLeft < 0){
+                        throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_TICKET_COUNT_ERROR);
+                    }
+                    dailyTrainTicket.setRw(countLeft);
+                }
+            }
+
+        }
     }
 
     public  void reduceTicketsReflect(SeatTypeEnum seatTypeEnum, DailyTrainTicket dailyTrainTicket) throws Exception {
@@ -164,36 +241,5 @@ public class ConfirmOrderService {
             setMethod.invoke(dailyTrainTicket, countLeft);
 
     }
-    private static void reduceTickets(SeatTypeEnum seatTypeEnum, DailyTrainTicket dailyTrainTicket) {
-        switch (seatTypeEnum){
-            case YDZ -> {
-                int countLeft = dailyTrainTicket.getYdz() - 1;
-                if (countLeft < 0){
-                    throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_TICKET_COUNT_ERROR);
-                }
-                dailyTrainTicket.setYdz(countLeft);
-            }
-            case EDZ -> {
-                int countLeft = dailyTrainTicket.getEdz() - 1;
-                if (countLeft < 0){
-                    throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_TICKET_COUNT_ERROR);
-                }
-                dailyTrainTicket.setEdz(countLeft);
-                
-            }case YW -> {
-                int countLeft = dailyTrainTicket.getYw() - 1;
-                if (countLeft < 0){
-                    throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_TICKET_COUNT_ERROR);
-                }
-                dailyTrainTicket.setYw(countLeft);
-                
-            }case RW -> {
-                int countLeft = dailyTrainTicket.getRw() - 1;
-                if (countLeft < 0){
-                    throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_TICKET_COUNT_ERROR);
-                }
-                dailyTrainTicket.setRw(countLeft);
-            }
-        }
-    }
+
 }
