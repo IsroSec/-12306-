19a13702fee3dac2bf772aebcf2e05dc5f29bc25
xikanqi <3051855262@qq.com>
2025-01-17 20:18:59 +0800

    12.23 更新确认订单为成功

--- business/src/main/java/com/jiawa/train/business/service/AfterConfirmOrderService.java
+++ business/src/main/java/com/jiawa/train/business/service/AfterConfirmOrderService.java
@@ -61,6 +61,9 @@ public class AfterConfirmOrderService {
     private DailyTrainTicketMapperCust dailyTrainTicketMapperCust;
     @Autowired
     private MemberFeign memberFeign;
+
+    @Autowired
+    private ConfirmOrderMapper confirmOrderMapper;
     /**
      *  选中座位后事务处理：
      *
@@ -71,7 +74,7 @@ public class AfterConfirmOrderService {
      * @param finalSeatList
      */
     @Transactional
-    public void AfterDoConfirm(DailyTrainTicket dailyTrainTicket,List<DailyTrainSeat> finalSeatList,List<ConfirmOrderTicketReq> tickets) {
+    public void AfterDoConfirm(DailyTrainTicket dailyTrainTicket,List<DailyTrainSeat> finalSeatList,List<ConfirmOrderTicketReq> tickets,ConfirmOrder confirmOrder) {
         for (int j=0;j<finalSeatList.size();j++) {
             DailyTrainSeat dailyTrainSeat = finalSeatList.get(j);
             DailyTrainSeat seatForUpdate = new DailyTrainSeat();
@@ -137,6 +140,12 @@ public class AfterConfirmOrderService {
             memberTicketReq.setSeatType(dailyTrainSeat.getSeatType());
             CommonResp<Object> commonResp = memberFeign.save(memberTicketReq);
             LOG.info("调用member接口，返回：{}",commonResp);
+
+            ConfirmOrder confirmOrderForUpdate = new ConfirmOrder();
+            confirmOrderForUpdate.setId(confirmOrder.getId());
+            confirmOrderForUpdate.setStatus(ConfirmOrderStatusEnum.SUCCESS.getCode());
+            confirmOrderForUpdate.setUpdateTime(new Date());
+            confirmOrderMapper.updateByPrimaryKeySelective(confirmOrderForUpdate);
         }
     }
 
