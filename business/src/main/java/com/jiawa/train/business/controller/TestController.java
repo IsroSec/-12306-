a09d9119c9d08344b057678bea862af8bab847b0
xikanqi <3051855262@qq.com>
2025-01-23 18:21:44 +0800

    17.6 关联模式和链路模式

--- business/src/main/java/com/jiawa/train/business/controller/TestController.java
+++ business/src/main/java/com/jiawa/train/business/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.jiawa.train.business.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.jiawa.train.business.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+    @Autowired
+    private TestService testService;
     @SentinelResource("hello")
     @GetMapping("/hello")
     public String hello() {
-        return "hello";
+        return testService.hello2();
+    }
+    @SentinelResource("hello1")
+    @GetMapping("/hello1")
+    public String hello1() {
+        return testService.hello2();
     }
 }
---
+++ business/src/main/java/com/jiawa/train/business/service/TestService.java
@@ -0,0 +1,21 @@
+package com.jiawa.train.business.service;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import org.springframework.stereotype.Service;
+
+/**
+ * ClassName: TestService
+ * Package: com.jiawa.train.business.service
+ * Description:
+ *
+ * @Author GalSec
+ * @Create 2025/1/23 17:57
+ * @Version 1.0
+ */
+@Service
+public class TestService {
+    @SentinelResource("hello2")
+    public String hello2() {
+        return "hello222222";
+    }
+}
